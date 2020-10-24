f036d724397c9901f65b02b42753294027184f98
zafar006 <73372893+zafar006@users.noreply.github.com>
2020-10-24 05:19:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('#menu-toggle');
+const menu = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', function (event) {
+    event.preventDefault();
+    menu.classList.toggle('visible');
+});
